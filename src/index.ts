5e2d10865a67e7416cc71b9498097d63a11d1a6f
0ritam <mujritam085@gmail.com>
2025-02-03 20:44:13 +0530

    validation and encryption

--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,91 @@
 import * as dotenv from "dotenv";
 dotenv.config();
-import express,{type Request, type Response} from "express";
+import express, { type Request, type Response } from "express";
 import cors from "cors";
 import { UserModel } from "./db";
 import mongoose from "mongoose";
-
+import { z } from "zod";
+import bcrypt, { hash } from "bcrypt";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-const port  = process.env.SERVER_PORT
+const port = process.env.SERVER_PORT;
 
-app.post("/api/v2/signup", async(req:Request, res: Response) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    try{
-        // const parsedBody = safeParse(req.body);
-        // if(parsedBody.error) {
-        //     throw new Error("Error occured while parsing teh JSON")
-        // }
+app.post("/api/v2/signup", async (req: Request, res: Response) => {
+  try {
+    const zodValidation = z.object({
+      username: z
+        .string()
+        .min(3, "enter min size 3 username")
+        .max(20, "too long"),
 
-        // const {username, password} = req.body;
+      password: z
+        .string()
+        .min(8, "to short")
+        .regex(
+          /[A-Z]/,
+          "password should contain atleast one uppercase character"
+        )
+        .regex(
+          /[a-z]/,
+          "password should contain atleast one lower case character"
+        )
+        .regex(/[0-9]/, "password should contain atleast one numeric character")
+        .regex(
+          /[\W_]/,
+          "password should contain atleast one special character"
+        ),
+    });
 
-        await UserModel.create({
-            username: username,
-            password: password
-        });
-        res.status(200).send("User signed up Successfully!") //cant send .status.json togetheer i tcretaes eerror of multiple header
-
-
-    } catch (err){
-        res.status(400).send("Pease enter Unique username");
-    } 
-})
-
-
-app.post("/api/v2/signin", (req:Request, res:Response)=>{
-
-})
-
-
-app.post("/api/v2/content", (req:Request, res:Response)=>{
-
-})
-
-app.get("/api/v2/content", (req:Request, res:Response)=>{
-
-})
-
-app.put("/api/v2/content/:contentId", (req:Request, res:Response)=>{
-
-})
-
-app.delete("/api/v2/content/:contentId", (req:Request, res:Response)=>{
-
-})
-
-app.post("/api/v2/brain/share", (req:Request, res:Response)=>{
-
-})
-
-app.post("/api/v2/brain/:shareLink", (req:Request, res:Response)=>{
-
-})
-
-const main = async () => {
-    if (typeof process.env.MONGODB_URL === "string") {
-        await mongoose.connect(process.env.MONGODB_URL);
-        console.log("Database connected...")
-    } else{
-        console.log("Error While Connecting...")
-
+    const parsedBody = zodValidation.safeParse(req.body);
+    if (parsedBody.error) {
+      throw new Error("Error occured while parsing teh JSON");
     }
 
-    app.listen(port, () => {
-        console.log(`app is listening in ${port}`);
-    })
-}
+    try {
+      const { username, password } = req.body;
+
+      const hashedPass = await bcrypt.hash(password ,5);
+
+      await UserModel.create({
+        username: username,
+        password: hashedPass,
+      });
+      res.status(200).send("User signed up Successfully!"); //cant send .status.json togetheer i tcretaes eerror of multiple header
+    } catch (err) {
+      res.status(400).send("Pease enter Unique username");
+    }
+  } catch (err) {
+    res.status(400).send(`Error occured...${err}`);
+  }
+});
+
+app.post("/api/v2/signin", (req: Request, res: Response) => {});
+
+app.post("/api/v2/content", (req: Request, res: Response) => {});
+
+app.get("/api/v2/content", (req: Request, res: Response) => {});
+
+app.put("/api/v2/content/:contentId", (req: Request, res: Response) => {});
+
+app.delete("/api/v2/content/:contentId", (req: Request, res: Response) => {});
+
+app.post("/api/v2/brain/share", (req: Request, res: Response) => {});
+
+app.post("/api/v2/brain/:shareLink", (req: Request, res: Response) => {});
+
+const main = async () => {
+  if (typeof process.env.MONGODB_URL === "string") {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Database connected...");
+  } else {
+    console.log("Error While Connecting...");
+  }
+
+  app.listen(port, () => {
+    console.log(`app is listening in ${port}`);
+  });
+};
 
 main();
