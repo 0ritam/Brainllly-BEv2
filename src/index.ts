14b28760514d621a64b40085a5a1b4bf447e5c1b
0ritam <mujritam085@gmail.com>
2025-02-05 22:15:00 +0530

    Backend Implemented

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express, { type Request, type Response } from "express";
 import cors from "cors";
-import { ContentModel, UserModel } from "./db";
+import { ContentModel, LinkModel, UserModel } from "./db";
 import mongoose from "mongoose";
 import { z } from "zod";
 import bcrypt, { hash } from "bcrypt";
 import jwt from "jsonwebtoken";
 import { userMiddleWare } from "./middleware";
+import { hashGenerator } from "./hashGenerator";
 
 const app = express();
 app.use(express.json());
@@ -120,15 +121,133 @@ app.post("/api/v2/content", userMiddleWare, async (req: Request, res: Response) 
   }
 });
 
-app.get("/api/v2/content", (req: Request, res: Response) => {});
+app.get("/api/v2/content",userMiddleWare, async (req: Request, res: Response) => {
 
-app.put("/api/v2/content/:contentId", (req: Request, res: Response) => {});
+  try{
+    const userId = req.userId;
+    const userConData = await ContentModel.find({
+      userId: userId,
+    }).populate("userId","username");
 
-app.delete("/api/v2/content/:contentId", (req: Request, res: Response) => {});
+    res.status(200).json({
+      userConData : userConData
+    })
 
-app.post("/api/v2/brain/share", (req: Request, res: Response) => {});
+  } catch (err){
+    res.status(400).send(`error occured while fetching content ${err}`)
 
-app.post("/api/v2/brain/:shareLink", (req: Request, res: Response) => {});
+  }
+});
+
+app.put("/api/v2/content/:contentId", userMiddleWare, async (req: Request, res: Response) => {
+  try{
+      const userId = req.userId;
+      const {type, link,title, tags} = req.body
+      const contentId = req.params.contentId;
+
+      if(!contentId) {
+        throw new Error("Please provide the content id to update the data");
+
+      }
+      const contentUpadation = await ContentModel.updateOne({
+        _id : contentId,
+        userId: userId,
+      },
+      {$set:{type, link,title, tags}}
+    );
+    res.status(200).send(`Upadated successfully`);
+  } catch (err){
+    res.status(500).send(`Error occured while updating ${err}`);
+
+  }
+});
+
+app.delete("/api/v2/content/:contentId", userMiddleWare,async (req: Request, res: Response) => {
+  try {
+    const userId = req.userId;
+    const contentId = req.params.contentId;
+
+    if(!contentId) {
+      throw new Error("Please provide the id to delete")
+    }
+    const result = await ContentModel.deleteOne({
+      _id: contentId,
+      userId: userId,
+    });
+    if (result.deletedCount === 0) {
+      res.status(400).send("You are not authorized to delete the data");
+      return;
+    }
+    res.status(200).send("Content delete for the given id successfully");
+  } catch(err){
+
+    res.status(400).send(`Error occured while deleting the content ${err}`);
+  }
+});
+
+app.post("/api/v2/brain/share",userMiddleWare,async(req: Request, res: Response) => {
+
+  try {
+    const share  = req.body.share;
+    if(share === true) {
+      const existingLink = await LinkModel.findOne({
+        userId:req.userId,
+      })
+
+      if(existingLink) {
+        res.status(200).json({
+          hash:existingLink.hash,
+        })
+        return;
+      }
+
+      const hash = hashGenerator(10);
+      const result = await LinkModel.create({
+        hash: hash,
+        userId: req.userId,
+      });
+      res.status(200).json({
+        message: "Hash generated successfully",
+        link: result.hash,
+      });
+      return;
+
+    } else if( share === false ) {
+      await LinkModel.deleteOne({
+        userId: req.userId,
+      });
+      res.status(200).send("hash Deleted successfully");
+    }
+  } catch(err) {
+    res.status(400).send(`Error occured while generating the hash ${err}`)
+
+  }
+
+});
+
+app.post("/api/v2/brain/:shareLink",userMiddleWare, async (req: Request, res: Response) => {
+  try {
+    const hash = req.params.shareLink;
+    const link = await LinkModel.findOne({
+      hash: hash,
+
+    });
+
+    if(!link) {
+      res.status(404).send("This link does not exits");
+      return;
+    }
+    const content = await ContentModel.find({
+      userId: link.userId,
+    }).populate("userId", "username");
+    res.status(400).json({
+      content
+    })
+  } catch(err){
+    res.status(400).send(`Eoor occured while loading the content ${err}`)
+
+  }
+});
 
 const main = async () => {
   if (typeof process.env.MONGODB_URL === "string") {
